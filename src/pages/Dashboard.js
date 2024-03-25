0997adcbc8cf9dfc80a001dae15d4a43f325dbe7
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2024-03-25 02:55:06 +0000

    Update Dashboard CSS
    Co-authored-by: AdhavChezhian <LimeTime311@users.noreply.github.com>

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,9 +2,11 @@ import "./Dashboard.css"
 
 function Statistics(){
     return <div className="stats-parent">
-        <h1>
-            10 Unmatched Students<br />15 Avaliable Tutors
-        </h1>    
+        <div>
+            <h1>
+                10 Unmatched Students<br />15 Avaliable Tutors
+            </h1>    
+        </div>
     </div>
 }
 
@@ -29,6 +31,7 @@ export default function Dashboard() {
     return(
         <div className="top">
             <Statistics />
+            <ViewBody />
         </div>
     );
 }
