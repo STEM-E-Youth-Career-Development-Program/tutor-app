ce92508d38439d38fed043ed2f5d1adf35dce6a0
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2024-04-01 02:09:55 +0000

    Navbar work
    Co-authored-by: masonzhangg <masonzhangg@users.noreply.github.com>
    Co-authored-by: anchalv00 <anchalv00@users.noreply.github.com>
    Co-authored-by: edencunningham <edencunningham@users.noreply.github.com>
    Co-authored-by: Alyssa C <alyssachou@users.noreply.github.com>
    Co-authored-by: GreaterthanGreat <GreaterthanGreat@users.noreply.github.com>

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,12 @@
+import "./index.css"
+
 export default function NavBar() {
     return <>
         <nav>
-            <a>Dashboard</a>
-            <a>Tutor List</a>
-            <a>Student List</a>
-            <img src = '#' alt='STEM-E logo'></img>
+            <div class="element"><a>Dashboard</a></div>
+            <div class="element"><a>Tutor List</a></div>
+            <div class="element"><a>Student List</a></div>
+            <img src = 'https://media.discordapp.net/attachments/814358449386160158/1224176088578789396/image.png?ex=661c89d3&is=660a14d3&hm=b0c4e075f28108bf49ce36b41364da96435185b9e5936f2a86d55d655a851e63&=&format=webp&quality=lossless&width=429&height=172' alt='STEM-E logo'></img>
         </nav>
     </>
 }
