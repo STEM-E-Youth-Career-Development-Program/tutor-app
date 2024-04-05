144b6266e4a4b3a848f43129854e4e22c448514f
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2024-04-05 00:19:50 +0000

    update navbar css

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,11 +2,13 @@ import "./index.css"
 
 export default function NavBar() {
     return <>
-        <nav>
-            <div class="element"><a>Dashboard</a></div>
-            <div class="element"><a>Tutor List</a></div>
-            <div class="element"><a>Student List</a></div>
-            <img src = 'https://media.discordapp.net/attachments/814358449386160158/1224176088578789396/image.png?ex=661c89d3&is=660a14d3&hm=b0c4e075f28108bf49ce36b41364da96435185b9e5936f2a86d55d655a851e63&=&format=webp&quality=lossless&width=429&height=172' alt='STEM-E logo'></img>
+        <nav class="navbar">
+            <ul>
+                <li><a href="/dashboard">Dashboard</a></li>
+                <li><a href="/">Tutor List</a></li>
+                <li><a href="/">Student List</a></li>
+                {/*<img src = 'https://media.discordapp.net/attachments/814358449386160158/1224176088578789396/image.png?ex=661c89d3&is=660a14d3&hm=b0c4e075f28108bf49ce36b41364da96435185b9e5936f2a86d55d655a851e63&=&format=webp&quality=lossless&width=429&height=172' alt='STEM-E logo'></img>*/}
+            </ul>
         </nav>
     </>
 }
