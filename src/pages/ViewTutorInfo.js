1cfc2216411950b76e6cedc66348199272eda586
anchalv00 <98239195+anchalv00@users.noreply.github.com>
2024-06-03 02:27:51 +0000

    Added Availability HTML

---
+++ src/pages/ViewTutorInfo.js
@@ -0,0 +1,61 @@
+import "./ViewTutorInfo.css";
+
+function Availability(){
+    return <> 
+        <div class="availability-table">
+            <table>
+                <tr className="day-headings">
+                    <th scope="col"></th>
+                    <th scope="col">Mon</th>
+                    <th scope="col">Tue</th>
+                    <th scope="col">Wed</th>
+                    <th scope="col">Thu</th>
+                    <th scope="col">Fri</th>
+                    <th scope="col">Sat</th>
+                    <th scope="col">Sun</th>
+                </tr>
+                <tr>
+                    <th scope="row">Morning</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">Afternoon</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <th scope="row">Evening</th>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </table>
+        </div>
+    </>
+}
+
+function AvailabilityChart(){
+    return<>
+        <div className="Headings">
+            <p>Available</p>
+            <p>Unavailable</p>
+        </div>
+    </>
+}
+
+export default ViewTutorInfo;
