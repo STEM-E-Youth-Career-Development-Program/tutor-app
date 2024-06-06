43afc3690c2fc0f37ed898cbb11cd5943b4277d0
Matthew1177 <56777713+Matthew1177@users.noreply.github.com>
2024-06-05 20:30:14 -0700

    Add base Tutor API + example

---
+++ src/firebaseApp.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDE8_f9WmJHgfOeLClzmgwgP_liTvf4Ul0",
+  authDomain: "stem-e-tutor-app-testing.firebaseapp.com",
+  projectId: "stem-e-tutor-app-testing",
+  storageBucket: "stem-e-tutor-app-testing.appspot.com",
+  messagingSenderId: "305310074723",
+  appId: "1:305310074723:web:e1256fd270efd038cbb556"
+};
+
+// eslint-disable-next-line no-unused-vars
+export const firebaseApp = initializeApp(firebaseConfig);
