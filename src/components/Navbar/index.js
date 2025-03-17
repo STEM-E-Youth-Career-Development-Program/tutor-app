124bbc1424f78f0b4c58669fa9f6f6bf55b74928
Kevin Diaz <kevindiaz.08.21.07@gmail.com>
2025-03-16 19:34:37 -0500

    Swapped students & tutors in matching algorithm

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,24 +3,41 @@ import "./index.css";
 
 export default function NavBar({ user, setUser }) {
     const navigate = useNavigate();
-    const exampleStudentId = "leFaNrKmmcXWjr6RvIPb";
+    const exampleStudentId = "4qK339SZdVGV0thmvoJJ";
 
-    return <>
-        <nav className="navbar">
-            <ul>
-                <li><Link to="/dashboard">Dashboard</Link></li>
-                <li><Link to="/view-tutors">Tutor List</Link></li>
-                <li><Link to="/view-students">Student List</Link></li>
-                {/*<img src = 'https://media.discordapp.net/attachments/814358449386160158/1224176088578789396/image.png?ex=661c89d3&is=660a14d3&hm=b0c4e075f28108bf49ce36b41364da96435185b9e5936f2a86d55d655a851e63&=&format=webp&quality=lossless&width=429&height=172' alt='STEM-E logo'></img>*/}
-                {user && 
-                    <li className="float-right" onClick={() => {
-                        setUser(undefined)
-                        navigate("/")
-                        // eslint-disable-next-line jsx-a11y/anchor-is-valid,no-script-url
-                    }}><a href="javascript:void(0)">Logout</a></li>
-                }
-            </ul>
-        </nav>
-    </>
+    return (
+        <>
+            <nav className="navbar">
+                <ul>
+                    <li>
+                        <Link to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li>
+                        <Link to="/view-tutors">Tutor List</Link>
+                    </li>
+                    <li>
+                        <Link to="/view-students">Student List</Link>
+                    </li>
+                    <li>
+                        <Link to={`/matching/${exampleStudentId}`}>
+                            Matching
+                        </Link>
+                    </li>
+                    {/*<img src = 'https://media.discordapp.net/attachments/814358449386160158/1224176088578789396/image.png?ex=661c89d3&is=660a14d3&hm=b0c4e075f28108bf49ce36b41364da96435185b9e5936f2a86d55d655a851e63&=&format=webp&quality=lossless&width=429&height=172' alt='STEM-E logo'></img>*/}
+                    {user && (
+                        <li
+                            className="float-right"
+                            onClick={() => {
+                                setUser(undefined);
+                                navigate("/");
+                                // eslint-disable-next-line jsx-a11y/anchor-is-valid,no-script-url
+                            }}
+                        >
+                            <a href="javascript:void(0)">Logout</a>
+                        </li>
+                    )}
+                </ul>
+            </nav>
+        </>
+    );
 }
-
