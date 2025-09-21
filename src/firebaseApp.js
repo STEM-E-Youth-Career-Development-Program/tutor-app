fa9ce4a7d0b2bb3c9423ebb754acb2be8e7fdda3
Alyssa C <63886676+alyssachou@users.noreply.github.com>
2025-09-20 23:28:15 -0400

    Update firebaseApp.js

--- src/firebaseApp.js
+++ src/firebaseApp.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDE8_f9WmJHgfOeLClzmgwgP_liTvf4Ul0",
-  authDomain: "stem-e-tutor-app-testing.firebaseapp.com",
-  projectId: "stem-e-tutor-app-testing",
-  storageBucket: "stem-e-tutor-app-testing.appspot.com",
-  messagingSenderId: "305310074723",
-  appId: "1:305310074723:web:e1256fd270efd038cbb556"
+    apiKey: "AIzaSyDSr52mwtEAYRnUIWfy2mESjDtR9qtUD1Y",
+    authDomain: "stem-e-tutor-app.firebaseapp.com",
+    projectId: "stem-e-tutor-app",
+    storageBucket: "stem-e-tutor-app.firebasestorage.app",
+    messagingSenderId: "246673595340",
+    appId: "1:246673595340:web:735d731a5f34af866b1b69"
 };
 
 // eslint-disable-next-line no-unused-vars
