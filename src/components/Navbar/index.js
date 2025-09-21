a58a81ad2743f0ad3ca1544a759090c23f19b035
Alyssa C <63886676+alyssachou@users.noreply.github.com>
2025-09-20 23:32:27 -0400

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -18,11 +18,6 @@ export default function NavBar({ user, setUser }) {
                     <li>
                         <Link to="/view-students">Student List</Link>
                     </li>
-                    <li>
-                        <Link to={`/matching/${exampleStudentId}`}>
-                            Matching
-                        </Link>
-                    </li>
                     {/*<img src = 'https://media.discordapp.net/attachments/814358449386160158/1224176088578789396/image.png?ex=661c89d3&is=660a14d3&hm=b0c4e075f28108bf49ce36b41364da96435185b9e5936f2a86d55d655a851e63&=&format=webp&quality=lossless&width=429&height=172' alt='STEM-E logo'></img>*/}
                     {user && (
                         <li
