a17e4b76a1ab65912251c603e65944559812b65e
devPenguin55 <aaravdeshmane@gmail.com>
2026-02-23 18:17:04 -0700

    Tutor filtering fixed with key capitalization

--- src/pages/ViewTutors.js
+++ src/pages/ViewTutors.js
@@ -143,7 +143,7 @@ function ViewTutors() {
     }
     const filteredRows = tutors.filter(tutor => {
         console.log(tutor);
-        const statusMatch = Object.keys(filters.status).every(key => !filters.status[key] || tutor.status === key);
+        const statusMatch = Object.keys(filters.status).every(key => !filters.status[key] || tutor.status === key.charAt(0).toUpperCase() + key.slice(1));
         const subjectsMatch = Object.keys(filters.subjects).every(key => !filters.subjects[key] || tutor.subjectTopics.includes(key));
         const gradesMatch = Object.keys(filters.grades).every(key => !filters.grades[key] || tutor.grade == key);
         const timezonesMatch = Object.keys(filters.timezones).every(key => !filters.timezones[key] || tutor.timezone === key);
